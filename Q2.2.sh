9695e5fc6c4e367aee0cdb6791d0f135b9fbae6f
Dylan Boyle <dylan96.boyle@gmail.com>
2024-04-17 23:52:32 +0000

    Command 2, 3, 4, 5, &6 - hostname, arch, uname -a, uptime & whoami

--- Q2.2.sh
+++ Q2.2.sh
@@ -1,7 +1,33 @@
-
+#!/bin/bash
 output_file="DylanBoyle.txt"
 
 echo "Date Command Output:" >> $output_file
 date >> output_file
 echo "----------------------------------------" >> $output_file
+
+# Command 2: hostname
+echo "Hostname Command Output:" >> $output_file
+hostname >> $output_file
+echo "----------------------------------------" >> $output_file
+
+# Command 3: arch
+echo "Architecture Command Output:" >> $output_file
+arch >> $output_file
+echo "----------------------------------------" >> $output_file
+
+# Command 4: uname -a
+echo "Uname Command Output:" >> $output_file
+uname -a >> $output_file
+echo "----------------------------------------" >> $output_file
+
+# Command 5: uptime
+echo "Uptime Command Output:" >> $output_file
+uptime >> $output_file
+echo "----------------------------------------" >> $output_file
+
+# Command 6: whoami
+echo "Whoami Command Output:" >> $output_file
+whoami >> $output_file
+echo "----------------------------------------" >> $output_file
+
 
